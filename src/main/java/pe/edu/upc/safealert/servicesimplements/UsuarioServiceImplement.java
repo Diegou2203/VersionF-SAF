0c7ceced3f94c35b25f626c87a22ebe84f02f8df
Diegou2203 <diegouribe990@gmail.com>
2025-05-11 14:21:31 -0500

    PUT POST USUARIO ENCRIPTADOS

--- src/main/java/pe/edu/upc/safealert/servicesimplements/UsuarioServiceImplement.java
+++ src/main/java/pe/edu/upc/safealert/servicesimplements/UsuarioServiceImplement.java
@@ -1,6 +1,7 @@
 package pe.edu.upc.safealert.servicesimplements;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import pe.edu.upc.safealert.entities.Usuario;
 import pe.edu.upc.safealert.repositories.IUsuarioRepository;
@@ -13,12 +14,17 @@ public class UsuarioServiceImplement implements IUsuarioService {
 
     @Autowired
     private IUsuarioRepository uR;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public List<Usuario> list() {return uR.findAll();}
 
     @Override
-    public void insert(Usuario u) {uR.save(u);}
+    public void insert(Usuario u) {
+        u.setPassword(passwordEncoder.encode(u.getPassword()));
+        uR.save(u);
+    }
 
     @Override
     public Usuario listarId(int idUsuario) {return uR.findById(idUsuario).orElse(new Usuario());}
@@ -27,7 +33,10 @@ public class UsuarioServiceImplement implements IUsuarioService {
     public void delete(int idUsuario) {uR.deleteById(idUsuario);}
 
     @Override
-    public void update(Usuario u) {uR.save(u);}
+    public void update(Usuario u) {
+        u.setPassword(passwordEncoder.encode(u.getPassword()));
+        uR.save(u);
+    }
 
     @Override
     public List<String[]> findUsuariosEnZonasDeAltoRiesgo() {
